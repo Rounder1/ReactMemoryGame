36eb757b5660246914774b5ef841afa91252c62d
Rounder1 <tolerdavid@gmail.com>
2018-06-18 21:43:24 -0400

    pics added

--- react-memory-game/src/components/Container.js
+++ react-memory-game/src/components/Container.js
@@ -4,7 +4,22 @@ const Container = () => (
 
 <div class="container">
     <div class= "col-sm-12">
-        
+
+    <img src={require('../pictures/aml-1.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-2.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-3.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-4.PNG')} alt="animal picture" />
+
+    <img src={require('../pictures/aml-5.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-6.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-7.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-8.PNG')} alt="animal picture" />
+
+    <img src={require('../pictures/aml-9.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-10.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-11.PNG')} alt="animal picture" />
+    <img src={require('../pictures/aml-12.PNG')} alt="animal picture" />
+
     </div>
 </div>
 
