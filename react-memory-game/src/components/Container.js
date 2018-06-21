179030777d6d0b49cccb349441d091e4812c4615
Rounder1 <tolerdavid@gmail.com>
2018-06-20 20:08:10 -0400

    random shuffle working

--- react-memory-game/src/components/Container.js
+++ react-memory-game/src/components/Container.js
@@ -1,24 +1,101 @@
 import React from "react";
 
+let images = [
+    {
+        path: require('../pictures/aml-1.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-2.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-3.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-4.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-5.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-6.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-7.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-8.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-9.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-10.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-11.PNG'),
+        selected: false,
+        inPlay: false
+    },
+    {
+        path: require('../pictures/aml-12.PNG'),
+        selected: false,
+        inPlay: false
+    }
+];
+
+function fisherYates (arr) {
+    var i = arr.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1)); 
+        temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    return arr;
+}
+
+let shuffled = fisherYates(images);
+
 const Container = () => (
 
 <div class="container">
     <div class= "col-sm-12">
 
-    <img src={require('../pictures/aml-1.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-2.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-3.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-4.PNG')} alt="animal picture" />
+    <img src={shuffled[0].path} alt="animal picture" />
+    <img src={shuffled[1].path} alt="animal picture" />
+    <img src={shuffled[2].path} alt="animal picture" />
+    <img src={shuffled[3].path} alt="animal picture" />
 
-    <img src={require('../pictures/aml-5.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-6.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-7.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-8.PNG')} alt="animal picture" />
+    <img src={shuffled[4].path} alt="animal picture" />
+    <img src={shuffled[5].path} alt="animal picture" />
+    <img src={shuffled[6].path} alt="animal picture" />
+    <img src={shuffled[7].path} alt="animal picture" />
 
-    <img src={require('../pictures/aml-9.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-10.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-11.PNG')} alt="animal picture" />
-    <img src={require('../pictures/aml-12.PNG')} alt="animal picture" />
+    <img src={shuffled[8].path} alt="animal picture" />
+    <img src={shuffled[9].path} alt="animal picture" />
+    <img src={shuffled[10].path} alt="animal picture" />
+    <img src={shuffled[11].path} alt="animal picture" />
+
 
     </div>
 </div>
